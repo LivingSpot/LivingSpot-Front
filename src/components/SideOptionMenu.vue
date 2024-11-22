cb2bccdc0aeef7d22d4d65497ab699a5b0766049
Mojin2 <aiduriaaa@gmail.com>
2024-11-22 11:11:56 +0900

    Remove node_modules from version control

---
+++ src/components/SideOptionMenu.vue
@@ -0,0 +1,519 @@
+<template>
+  <div v-if="route.name === 'main'" class="h-full w-full">
+    <div
+      v-if="showForm"
+      class="h-full flex flex-col justify-center bg-gradient-to-br from-white to-gray-100 p-6 rounded-lg shadow-lg"
+    >
+      <h2 class="text-2xl font-bold text-center mb-6 text-gray-800">
+        상세 검색
+      </h2>
+      <form class="space-y-6">
+        <!-- 지역 선택 -->
+        <div>
+          <label class="block text-gray-700 font-medium mb-2">지역 정보</label>
+          <div class="flex gap-y-2 flex-col">
+            <select
+              v-model="selectedOptions.sido"
+              @change="onSelectChange('sido')"
+              class="w-full px-4 py-3 rounded-md bg-white border border-gray-300 shadow-sm focus:ring focus:ring-blue-300"
+            >
+              <option value="">시도선택</option>
+              <option
+                v-for="option in findSelectOptions('sido')"
+                :key="option.value"
+                :value="option.value"
+              >
+                {{ option.text }}
+              </option>
+            </select>
+            <select
+              v-model="selectedOptions.gugun"
+              @change="onSelectChange('gugun')"
+              class="w-full px-4 py-3 rounded-md bg-white border border-gray-300 shadow-sm focus:ring focus:ring-blue-300"
+            >
+              <option value="">구군선택</option>
+              <option
+                v-for="option in findSelectOptions('gugun')"
+                :key="option.value"
+                :value="option.value"
+              >
+                {{ option.text }}
+              </option>
+            </select>
+            <select
+              v-model="selectedOptions.dong"
+              class="w-full px-4 py-3 rounded-md bg-white border border-gray-300 shadow-sm focus:ring focus:ring-blue-300"
+            >
+              <option value="">동선택</option>
+              <option
+                v-for="option in findSelectOptions('dong')"
+                :key="option.value"
+                :value="option.value"
+              >
+                {{ option.text }}
+              </option>
+            </select>
+          </div>
+        </div>
+
+        <!-- 날짜 선택 -->
+        <div>
+          <label class="block text-gray-700 font-medium mb-2">날짜 정보</label>
+          <div class="flex space-x-4">
+            <select
+              v-model="selectedOptions.year"
+              @change="onSelectChange('year')"
+              class="w-1/2 px-4 py-3 rounded-md bg-white border border-gray-300 shadow-sm focus:ring focus:ring-blue-300"
+            >
+              <option value="">매매년선택</option>
+              <option
+                v-for="option in findSelectOptions('year')"
+                :key="option.value"
+                :value="option.value"
+              >
+                {{ option.text }}
+              </option>
+            </select>
+            <select
+              v-model="selectedOptions.month"
+              class="w-1/2 px-4 py-3 rounded-md bg-white border border-gray-300 shadow-sm focus:ring focus:ring-blue-300"
+            >
+              <option value="">매매월선택</option>
+              <option
+                v-for="option in findSelectOptions('month')"
+                :key="option.value"
+                :value="option.value"
+              >
+                {{ option.text }}
+              </option>
+            </select>
+          </div>
+        </div>
+
+        <!-- 아파트 이름 -->
+        <div>
+          <label class="block text-gray-700 font-medium mb-2">이름 정보</label>
+          <input
+            type="text"
+            v-model="aptNameInput"
+            placeholder="이름으로 검색.."
+            class="w-full px-4 py-3 rounded-md bg-white border border-gray-300 shadow-sm focus:ring focus:ring-blue-300"
+          />
+        </div>
+
+        <!-- 검색 버튼 -->
+        <div class="text-center">
+          <button
+            type="button"
+            @click="onSearchDetail"
+            class="px-6 py-3 text-white bg-blue-500 hover:bg-blue-600 rounded-lg shadow-md transition"
+          >
+            검색하기
+          </button>
+        </div>
+      </form>
+    </div>
+    <!-- 검색 결과 -->
+    <div v-else class="w-full px-2 h-full overflow-hidden">
+      <!-- 검색 결과 헤더 -->
+      <div
+        class="relative h-[7%] font-extrabold text-xl flex items-center justify-center mb-3 mt-10"
+      >
+        <font-awesome-icon
+          icon="fa-solid fa-arrow-left"
+          class="absolute left-2 cursor-pointer"
+          @click="onBackToSearch"
+        />
+        검색 결과
+      </div>
+
+      <!-- 결과 리스트 -->
+      <div class="h-full overflow-y-auto scrollbar-hide -mt-5">
+        <v-infinite-scroll
+          color="secondary"
+          height="730"
+          @load="load"
+          class="flex gap-y-2 h-full scrollbar-hide"
+        >
+          <ResultCard v-for="list in displayedList" :data="list" />
+          <!-- <ResultCard :data="list" />
+        </ResultCard> -->
+          <template v-slot:empty>
+            <v-alert type="warning">No more items!</v-alert>
+          </template>
+          <!-- <ResultCard v-for="item in displayedList" :key="item.id" :data="item" /> -->
+        </v-infinite-scroll>
+      </div>
+    </div>
+  </div>
+  <div v-else class="h-full w-full">
+    <div class="w-full px-2 h-full overflow-hidden">
+      <!-- 검색 결과 헤더 -->
+      <div
+        class="relative h-[7%] font-extrabold text-xl flex items-center justify-center mb-4 mt-10"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'fa-heart']"
+          class="mr-2 text-red-500"
+        />
+        찜 목록
+        <font-awesome-icon
+          :icon="['fas', 'fa-heart']"
+          class="ml-2 text-red-500"
+        />
+      </div>
+
+      <!-- 결과 리스트 -->
+      <div
+        class="overflow-y-auto h-[730px] scrollbar-hide -mt-5 gap-y-2 flex flex-col"
+      >
+        <ResultCard v-for="list in tmpList" :data="list" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, onMounted, onUnmounted, nextTick } from "vue";
+import { searchDetail } from "@/api/search.js";
+import ResultCard from "@/components/ResultCard.vue";
+import { useRouter, useRoute } from "vue-router";
+
+import "v3-infinite-loading/lib/style.css";
+
+const router = useRouter();
+const route = useRoute();
+const isOpened = ref(false);
+
+// 상태 변수들
+const showForm = ref(true);
+const list = ref([]); // 전체 데이터
+const displayedList = ref([]); // 화면에 보여질 데이터
+const currentPage = ref(1); // 현재 페이지
+const itemsPerPage = ref(5); // 페이지당 아이템 수
+const isLoading = ref(false); // 로딩 상태
+const hasMore = ref(true); // 추가 데이터 여부
+const aptNameInput = ref("");
+const maxLen = ref(0);
+const isLoadingFinish = ref(false); // 더 이상 로딩할 것이 없다는 것
+const start = ref(5);
+const end = ref(10);
+
+const load = ({ done }) => {
+  setTimeout(() => {
+    if (isLoadingFinish.value) {
+      done("empty");
+    } else {
+      console.log("MAXLENGTH ::: " + maxLen.value);
+      if (end.value > maxLen.value) {
+        end.value = maxLen.value;
+        isLoadingFinish.value = true;
+      }
+      displayedList.value.push(...list.value.slice(start.value, end.value));
+      console.log("displayedList ::: " + displayedList.value);
+      start.value = end.value;
+      end.value += itemsPerPage.value;
+      done("ok"); // Indicates that loading is complete
+    }
+  }, 1000);
+};
+const selectedOptions = reactive({
+  sido: "",
+  gugun: "",
+  dong: "",
+  year: "",
+  month: "",
+});
+
+const tmpList = reactive([
+  {
+    no: 1,
+    aptSeq: "11110-2224",
+    aptDong: " ",
+    floor: "9",
+    dealYear: 2011,
+    dealMonth: 11,
+    dealDay: 26,
+    excluUseAr: 59.92,
+    dealAmount: "38,000",
+    houseInfo: {
+      aptSeq: "11110-2224",
+      sggCd: "11110",
+      umdCd: "17500",
+      umdNm: "숭인동",
+      jibun: "2-1",
+      roadNmSggCd: "11110",
+      roadNm: "동망산길",
+      roadNmBonbun: "47",
+      roadNmBubun: "0",
+      aptNm: "종로센트레빌",
+      buildYear: 2008,
+      latitude: "37.5815574293752",
+      longitude: "127.015738287163",
+    },
+  },
+  {
+    no: 2,
+    aptSeq: "11110-31",
+    aptDong: " ",
+    floor: "3",
+    dealYear: 2011,
+    dealMonth: 11,
+    dealDay: 24,
+    excluUseAr: 83.83,
+    dealAmount: "26,500",
+    houseInfo: {
+      aptSeq: "11110-31",
+      sggCd: "11110",
+      umdCd: "17400",
+      umdNm: "창신동",
+      jibun: "578-5",
+      roadNmSggCd: "11110",
+      roadNm: "창신길",
+      roadNmBonbun: "20",
+      roadNmBubun: "0",
+      aptNm: "동대문맨션",
+      buildYear: 1973,
+      latitude: "37.5723822385791",
+      longitude: "127.011010638393",
+    },
+  },
+  {
+    no: 3,
+    aptSeq: "11110-86",
+    aptDong: " ",
+    floor: "3",
+    dealYear: 2011,
+    dealMonth: 11,
+    dealDay: 28,
+    excluUseAr: 84.84,
+    dealAmount: "33,000",
+    houseInfo: {
+      aptSeq: "11110-86",
+      sggCd: "11110",
+      umdCd: "18600",
+      umdNm: "신영동",
+      jibun: "254",
+      roadNmSggCd: "11110",
+      roadNm: "진흥로",
+      roadNmBonbun: "480",
+      roadNmBubun: "0",
+      aptNm: "대아파크빌",
+      buildYear: 1998,
+      latitude: "37.6058401893334",
+      longitude: "126.960276209248",
+    },
+  },
+  {
+    no: 4,
+    aptSeq: "11110-91",
+    aptDong: " ",
+    floor: "8",
+    dealYear: 2011,
+    dealMonth: 11,
+    dealDay: 1,
+    excluUseAr: 79.87,
+    dealAmount: "33,500",
+    houseInfo: {
+      aptSeq: "11110-91",
+      sggCd: "11110",
+      umdCd: "17400",
+      umdNm: "창신동",
+      jibun: "703",
+      roadNmSggCd: "11110",
+      roadNm: "낙산길",
+      roadNmBonbun: "198",
+      roadNmBubun: "0",
+      aptNm: "창신쌍용2",
+      buildYear: 1993,
+      latitude: "37.5803962235982",
+      longitude: "127.011011705247",
+    },
+  },
+  {
+    no: 5,
+    aptSeq: "11110-207",
+    aptDong: " ",
+    floor: "6",
+    dealYear: 2011,
+    dealMonth: 11,
+    dealDay: 28,
+    excluUseAr: 56.17,
+    dealAmount: "12,400",
+    houseInfo: {
+      aptSeq: "11110-207",
+      sggCd: "11110",
+      umdCd: "17400",
+      umdNm: "창신동",
+      jibun: "436-79",
+      roadNmSggCd: "11110",
+      roadNm: "청계천로",
+      roadNmBonbun: "295",
+      roadNmBubun: "0",
+      aptNm: "(436-79)",
+      buildYear: 1968,
+      latitude: "37.5700667120798",
+      longitude: "127.010670484701",
+    },
+  },
+]);
+
+const selects = reactive([
+  { id: "sido", defaultText: "시도선택", options: [] },
+  { id: "gugun", defaultText: "구군선택", options: [] },
+  { id: "dong", defaultText: "동선택", options: [] },
+  {
+    id: "year",
+    defaultText: "매매년선택",
+    options: generateYearOptions(),
+  },
+  { id: "month", defaultText: "매매월선택", options: [] },
+]);
+
+// 데이터 초기화 및 로드
+onMounted(() => {
+  fetchRegionData("sido", "*00000000");
+});
+
+// 검색 버튼 클릭
+async function onSearchDetail() {
+  try {
+    // 검색 API 호출
+    const params = {
+      dong: selectedOptions.dong || "",
+      aptName: aptNameInput.value || "",
+      year: selectedOptions.year || -1,
+      month: selectedOptions.month || -1,
+    };
+
+    const response = await searchDetail(
+      params.dong,
+      params.aptName,
+      params.year,
+      params.month
+    );
+
+    list.value = response.data; // 전체 데이터를 저장
+    // console.log(list.value);
+    console.log("FETCH 해온 결과 리스트의 길이 ::: " + list.value.length);
+    maxLen.value = list.value.length;
+    displayedList.value = list.value.slice(0, itemsPerPage.value); // 첫 5개 데이터 표시
+    hasMore.value = list.value.length > itemsPerPage.value;
+    showForm.value = false;
+  } catch (error) {
+    console.error("검색 실패:", error);
+    alert("검색 실패: 다시 시도해주세요.");
+  }
+}
+
+// // 옵션 초기화
+function resetOptions(type) {
+  if (type === "sido") {
+    selects.find((select) => select.id === "gugun").options = [
+      { value: "", text: "구군선택" },
+    ];
+    selects.find((select) => select.id === "dong").options = [
+      { value: "", text: "동선택" },
+    ];
+  } else if (type === "gugun") {
+    selects.find((select) => select.id === "dong").options = [
+      { value: "", text: "동선택" },
+    ];
+  }
+}
+
+// // 지역 데이터 API 호출
+function fetchRegionData(type, regcode) {
+  const url = `https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern=${regcode}&is_ignore_zero=true`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => updateSelectOptions(type, data.regcodes));
+}
+// // 옵션 갱신
+function updateSelectOptions(type, regcodes) {
+  const options = regcodes.map((regcode) => ({
+    value: regcode.code,
+    text: formatRegionName(type, regcode.name),
+  }));
+  const targetSelect = selects.find((select) => select.id === type);
+  if (targetSelect)
+    targetSelect.options = [
+      { value: "", text: targetSelect.defaultText },
+      ...options,
+    ];
+}
+
+// // 선택 변경 처리
+function onSelectChange(type) {
+  if (type === "year") {
+    selects.find((select) => select.id === "month").options =
+      generateMonthOptions(selectedOptions.year);
+  } else if (type === "sido" && selectedOptions.sido) {
+    fetchRegionData("gugun", `${selectedOptions.sido.substr(0, 2)}*00000`);
+  } else if (type === "gugun" && selectedOptions.gugun) {
+    fetchRegionData("dong", `${selectedOptions.gugun.substr(0, 5)}*`);
+  } else {
+    resetOptions(type);
+  }
+}
+
+// // 유틸리티 함수
+function generateYearOptions() {
+  const currentYear = new Date().getFullYear();
+  return Array.from({ length: 20 }, (_, i) => {
+    const year = currentYear - i;
+    return { value: year, text: `${year}년` }; // 템플릿 리터럴로 수정
+  });
+}
+
+function generateMonthOptions(year) {
+  const currentYear = new Date().getFullYear();
+  const maxMonth = year == currentYear ? new Date().getMonth() + 1 : 12;
+  return Array.from({ length: maxMonth }, (_, i) => {
+    const month = i + 1;
+    return { value: month < 10 ? `0${month}` : month, text: `${month}월` };
+  });
+}
+
+function formatRegionName(type, name) {
+  const nameParts = name.split(" ");
+  if (type === "sido") return nameParts[0];
+  if (type === "gugun") {
+    return nameParts.length === 2
+      ? nameParts[1]
+      : `${nameParts[1]} ${nameParts[2]}`; // 템플릿 리터럴로 수정
+  }
+  return nameParts.length === 3 ? nameParts[2] : nameParts[3];
+}
+
+function findSelectOptions(type) {
+  return selects.find((select) => select.id === type)?.options || [];
+}
+
+function onBackToSearch() {
+  showForm.value = true;
+  isLoadingFinish.value = false; // 더 이상 로딩할 것이 없다는 것
+  start.value = 5;
+  end.value = 10;
+  itemsPerPage.value = 5;
+}
+</script>
+
+<style>
+.custom-select,
+.custom-input {
+  flex: 1;
+  padding: 0.75rem;
+  font-size: 1rem;
+  border: 1px solid #eaeaea;
+  border-radius: 8px;
+  background: #ffffff;
+  transition: border-color 0.3s ease;
+}
+
+.custom-select:focus,
+.custom-input:focus {
+  border-color: #007bff;
+  outline: none;
+}
+</style>
